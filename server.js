a20ca82f9ab6749b29d673ab5a1d03f60a8e07f0
MasterDipp5 <masterdipp5@gmail.com>
2025-04-02 21:49:22 +0200

    Testing

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
 const app = express();
 
-let currentGifUrl = "https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif"; // default
-
+app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
+let currentGifUrl = "https://media.giphy.com/media/xT9IgG50Fb7Mi0prBC/giphy.gif";
 
 app.post('/set-gif', (req, res) => {
     const { gifUrl } = req.body;
-    if (gifUrl) {
-        currentGifUrl = gifUrl;
-        console.log("Updated GIF:", currentGifUrl);
-        res.sendStatus(200);
-    } else {
-        res.status(400).send("gifUrl is required");
-    }
+    if (!gifUrl) return res.status(400).send("Missing gifUrl");
+    currentGifUrl = gifUrl;
+    res.sendStatus(200);
 });
 
 app.get('/current-gif', (req, res) => {
@@ -24,5 +20,5 @@ app.get('/current-gif', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`🎥 OBS Background Controller running at http://localhost:${PORT}`);
+    console.log(`🔥 Server running on port ${PORT}`);
 });
